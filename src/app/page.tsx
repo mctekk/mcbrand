b611444fd2e892993536193f801672887888bd97
José David Sánchez <49158320+Arzoid29@users.noreply.github.com>
2023-09-29 14:54:01 -0400

    feat: adding the header or navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import { translate } from "@/locales";
+
+import NavBar from "@/components/organism/navBar";
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>{translate('base.hello')}</h1>
+    <main >
+      <NavBar/>
     </main>
   )
 }
---
+++ src/components/molecules/menu/index.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import  { MenuItem } from "../../atoms/menuItem";
+import { translate } from "@/locales";
+
+
+const menuItems = [
+  {
+    text: translate("navbar.section.whySalesAssist"),
+    link: "/why-sales-assist",
+  },
+  { text: translate("navbar.section.pricing"), link: "/pricing" },
+  { text: translate("navbar.section.aboutUs"), link: "/about-us" },
+];
+const externalLinks = [
+  {
+    text: translate("navbar.section.login"),
+    link: "https://app.salesassist.io/",
+    isExternal: true,
+    className: "hidden lg:flex"
+  },
+  {
+    text: translate("navbar.section.bookDemo"),
+    link: "https://calendly.com/salesasssit/30min",
+    isExternal: true,
+    className: "lg:bg-orange-500 lg:text-white bg-white "
+  },
+  
+];
+
+function Menu({ }) {
+  return (
+    <ul className="lg:flex">
+      {menuItems.map((item, index) => (
+        <MenuItem key={index} title={item.text} link={item.link}  />
+      ))}
+      <div className="lg:ml-80 flex lg:space-x-2">
+        {externalLinks.map((item, index) => (
+          <MenuItem key={index} title={item.text} link={item.link} isExternal={item.isExternal} className={item.className}  />
+        ))}
+      </div>
+    </ul>
+  );
+}
+
+export default Menu;
