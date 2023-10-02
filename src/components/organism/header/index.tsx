c9ef82db450f7aadcaf5e1c69e9d5008619aeffa
José David Sánchez <49158320+Arzoid29@users.noreply.github.com>
2023-10-02 09:51:34 -0400

    Fix:Responsive header

--- src/components/organism/header/index.tsx
+++ src/components/organism/header/index.tsx
@@ -49,7 +49,7 @@ function Header() {
           >
             <div className="fixed inset-0 bg-black bg-opacity-25" />
           </Transition.Child>
-
+          <Dialog.Panel>
           <nav
             className={`${
               isOpen
@@ -65,21 +65,8 @@ function Header() {
 
             <Menu  />
           </nav>
-          <div className="fixed inset-0 overflow-y-auto">
-            <div className="flex min-h-full items-center justify-center p-4 text-center">
-              <Transition.Child
-                as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0 scale-95"
-                enterTo="opacity-100 scale-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100 scale-100"
-                leaveTo="opacity-0 scale-95"
-              >
-                <Dialog.Panel className="w-full hidden max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all"></Dialog.Panel>
-              </Transition.Child>
-            </div>
-          </div>
+          </Dialog.Panel>
+          
         </Dialog>
       </Transition>
     </div>
