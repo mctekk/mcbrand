6946308b25f6e90845bbdc0563ef445f0c6c3053
José David Sánchez <49158320+Arzoid29@users.noreply.github.com>
2023-10-03 15:46:50 -0400

    Fix: fixing a type

--- src/components/organism/sections/home/hero/index.tsx
+++ src/components/organism/sections/home/hero/index.tsx
@@ -6,6 +6,7 @@ import { translate } from "@/locales";
 
 import { Typewriter } from "react-simple-typewriter";
 
+
 export const Hero = () => {
   return (
     <div className="section ">
@@ -23,7 +24,7 @@ export const Hero = () => {
                 <div className="overflow-hidden gap-6 flex flex-col h-[50px] md:h-[90px] lg:h-[99px] slices  ">
                   <h1 className="text-orange-500 md:text-[3rem] lg:text-[4.5rem] slice ml-5 md:ml-0 lg:ml-0 ">
                     <Typewriter
-                      words={translate("home.modules")}
+                      words={Array.from(translate("home.modules"))}
                       loop={0}
                       cursor
                       cursorStyle="_"
