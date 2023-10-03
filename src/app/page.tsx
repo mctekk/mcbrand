c497679d3a9986b93bbbba7fd843d48bfa2a3043
José David Sánchez <49158320+Arzoid29@users.noreply.github.com>
2023-10-03 14:01:57 -0400

    Feat: adding the hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "@/components/organism/header";
+import { Hero } from "./sections/home-sections";
 
 export default function Home() {
   return (
     <main >
       <Header/>
+      <Hero></Hero>
     </main>
   )
 }
