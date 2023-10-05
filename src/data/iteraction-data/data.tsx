2bc882845b6973e73be7ea15d54670a67afd9156
José David Sánchez <49158320+Arzoid29@users.noreply.github.com>
2023-10-04 22:09:52 -0400

    Feat: adding the iteracton section

---
+++ src/data/iteraction-data/data.tsx
@@ -0,0 +1,63 @@
+export const data = [
+  {
+    name: "Get Docs",
+    title: "Collect documents in real-time",
+    desc: "Allow customers to upload documents on the spot with their mobile device. Stop chasing customers to scan and upload documents and increase the speed and completion rate.",
+    url: "/images/actions/Get-Docs.webp"
+  },
+  {
+    name: "e-Form",
+    title: "Make all your forms smart",
+    desc: "Reduce unnecessary errors, increase the completion rate and shorten the form fill time, with forms that are made for mobile completion and are fully integrated with all of your existing systems.",
+    url: "/images/actions/e-Forms.webp"
+  },
+  {
+    name: "e-Signature",
+
+    title: "Make e-Signatures fast, easy, mobile",
+    desc: "Send customers agreements for e-Signatures that are designed for mobile completion. Complete sales agreements in real time, with much higher completion rates than legacy eSign.",
+    url: "/images/actions/e-Signature.webp"
+  },
+  {
+    name: "Product Sharing",
+
+    title: "Better remote sales",
+    desc: "Share product visuals and details while speaking with customers to ensure they are fully informed and excited about what they're purchasing.",
+    url: "/images/actions/Product-Sharing.webp"
+  },
+  {
+    name: "Request Referrals",
+
+    title: "Maximize each relationship",
+    desc: "Make it easy for customers to refer friends and family. As soon as customers share a referral a new opportunity is created and assigned in your CRM.",
+    url: "/images/actions/Request-Referrals.webp"
+  },
+  {
+    name: "Get Reviews",
+
+    title: "Easily collect reviews",
+    desc: "Make it extremely easy for your customers to leave a review. Send personalized text/email links that customers can tap and instantly leave reviews on their platform of choice.",
+    url: "/images/actions/Get-Reviews.webp"
+  },
+  {
+    name: "Secure Payments",
+
+    title: "Collect payments seamlessly",
+    desc: "Collect payments in real-time without web redirects or inefficient IVR systems. Using personalized payment links, quickly and securely collect payments wherever your customers and salespeople are.",
+    url: "/images/actions/Secure-Payments.webp"
+  },
+  {
+    name: "ID Verification",
+
+    title: "Add convenient security",
+    desc: "Stay secure without adding friction to your sales process. Use real-time ID verification straight from your customer's smartphones, eliminating the need for in-person visits or emailing of documents.",
+    url: "/images/actions/ID-Verification.webp"
+  },
+  {
+    name: "Digital Consent",
+
+    title: "Gain fast, easy and compliant consent",
+    desc: "Eliminate the time consuming and error-prone script reading. Send customers a T&C they can accept immediately (e.g., through checkboxes) right from their mobile device.",
+    url: "/images/actions/Digital Consent.webp"
+  }
+];
