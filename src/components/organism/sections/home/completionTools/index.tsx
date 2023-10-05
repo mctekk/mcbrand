419e568806ae5b15df0eb6ee7c640a5bb3fb1950
José David Sánchez <49158320+Arzoid29@users.noreply.github.com>
2023-10-04 22:17:15 -0400

    Fix: adding missing code

--- src/components/organism/sections/home/completionTools/index.tsx
+++ src/components/organism/sections/home/completionTools/index.tsx
@@ -1,24 +1,24 @@
 import Image from "next/image";
 import { Section } from "@/components/atoms/section";
-
-
+import { translate } from "@/locales";
 export default function CompletionTools() {
   return (
     <div className="section bg-white !pb-0 xl:pl-36 pl-0 md:pl-0">
+      {" "}
       <Section className="flex flex-col gap-6 w-full lg:px-16 xl:w-[80rem]   ">
-
+        {" "}
         <h1 className="text-[2.125rem]">
-          Embedded In and Fully Integrated With Your CRM
-        </h1>
-        <h4 className="mb-6 ">
-          Have a complete arsenal of every possible info-gathering widget in a
-          single panel, right from your CRM. All information and documents
-          gathered enter your CRM in real time, with audit trail.
-        </h4>
-
-        <Image alt="Completion Tools" src="/images/completion-tools-screenshot.png" width={2000} height={24}/>
-      </Section>
+          {" "}
+          {translate("home.completiontools.title")}{" "}
+        </h1>{" "}
+        <h4 className="mb-6 "> {translate("home.completiontools.desc")} </h4>{" "}
+        <Image
+          alt="Completion Tools"
+          src="/images/completion-tools-screenshot.png"
+          width={2000}
+          height={24}
+        />{" "}
+      </Section>{" "}
     </div>
   );
-};
-
+}
