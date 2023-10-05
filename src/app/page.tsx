3b1f94c0e0e9e503341737c963ca7720f84a2611
José David Sánchez <49158320+Arzoid29@users.noreply.github.com>
2023-10-05 18:44:02 -0400

    Feat: adding the completionProcess section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import CompletionTools from "@/components/organism/sections/home/completionTools
 import ImageChanger from "@/components/organism/sections/home/iteraction";
 import { data } from "@/data/iteraction-data/data";
 import { Automated } from "@/components/organism/sections/home/automated";
+import { CompletionProcess } from "@/components/organism/sections/home/completionProcess";
 
 
 export default function Home() {
@@ -14,6 +15,7 @@ export default function Home() {
       <CompletionTools/>
       <ImageChanger data={data}/>
       <Automated></Automated>
+      <CompletionProcess></CompletionProcess>
     </main>
   )
 }
