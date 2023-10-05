1ad90f2fb2c810d117a1ddd4c492dc073f3fa933
José David Sánchez <49158320+Arzoid29@users.noreply.github.com>
2023-10-04 22:15:23 -0400

    Fix: using translate

--- src/components/organism/sections/home/completionTools/index.tsx
+++ src/components/organism/sections/home/completionTools/index.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Section } from "@/components/atoms/section";
+import { translate } from "@/locales";
 
 
 export default function CompletionTools() {
@@ -8,12 +9,10 @@ export default function CompletionTools() {
       <Section className="flex flex-col gap-6 w-full lg:px-16 xl:w-[80rem]   ">
 
         <h1 className="text-[2.125rem]">
-          Embedded In and Fully Integrated With Your CRM
+          {translate("home.completiontools.title")}
         </h1>
         <h4 className="mb-6 ">
-          Have a complete arsenal of every possible info-gathering widget in a
-          single panel, right from your CRM. All information and documents
-          gathered enter your CRM in real time, with audit trail.
+        {translate("home.completiontools.desc")}
         </h4>
 
         <Image alt="Completion Tools" src="/images/completion-tools-screenshot.png" width={2000} height={24}/>
