d7a0b63cee1950e815eff20ac899459f2c19f2e0
José David Sánchez <49158320+Arzoid29@users.noreply.github.com>
2023-10-10 13:25:45 -0400

    fix:identation

--- src/components/atoms/leading/index.tsx
+++ src/components/atoms/leading/index.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 
-
 interface LeadingProps {
-    title: string;
-    content: string;
-  }
+  title: string;
+  content: string;
+}
 
-
-  export default function Leading({ title, content }: LeadingProps) {
-    return (
-      <div>
-        <h3 className='font-semibold text-[1.5rem] leading-6'>{title}</h3>
-        <span className='flex justify-center leading-6 text-gray-400 font-medium text-[1.3rem]'>{content}</span>
-      </div>
-    );
-  }
+export default function Leading({ title, content }: LeadingProps) {
+  return (
+    <div>
+      <h3 className="font-semibold text-[1.5rem] leading-6">{title}</h3>
+      <span className="flex justify-center leading-6 text-gray-400 font-medium text-[1.3rem]">
+        {content}
+      </span>
+    </div>
+  );
+}
