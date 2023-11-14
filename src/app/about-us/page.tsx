76a5aa52eda4b88094fc7d912f16fe82f03b84ec
José David Sánchez <49158320+Arzoid29@users.noreply.github.com>
2023-11-14 17:43:16 -0400

    fix: routes

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -61,6 +61,7 @@ const PAGE_SECTIONS_ABOUT_US: Record<string, ReactNode> = {
       img="/images/Frame.jpg"
       button={false}
       backColor="bg-white"
+      buttonLink=""
     ></InfoSection>,
     <InfoSection
       desc="Whether it's enhancing E-commerce platforms, optimizing inventory management systems, or elevating client portals, our extensive experience with a diverse clientele has equipped us to tackle your software-related challenges effectively, enabling you to concentrate on your company's overarching objectives."
@@ -68,6 +69,7 @@ const PAGE_SECTIONS_ABOUT_US: Record<string, ReactNode> = {
       button={false}
       reverse={true}
       backColor="bg-white"
+      buttonLink=""
     ></InfoSection>,
     <Brands imageUrls={imagesBrand} title="Brands that trust us" titleColor="text-white" />,
    <Behind></Behind>,
