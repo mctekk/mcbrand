26f4a1cdd8ae7a0ccd2996974b342d3fea2a5ca3
José David Sánchez <49158320+Arzoid29@users.noreply.github.com>
2024-01-04 16:58:47 -0400

    fix: using the logic to all blogs

--- src/app/posts/[postSlug]/page.tsx
+++ src/app/posts/[postSlug]/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import PostClient from "./client";
 import { Metadata, ResolvingMetadata } from "next";
 import api from "@/model/api/dato-cms/data";
+
 type Props = {
   params: { postSlug: string };
   searchParams?: { [key: string]: string | string[] | undefined };
@@ -15,76 +16,64 @@ export async function generateMetadata(
   console.log(id);
   const pageType = process.env.NEXT_PUBLIC_PAGE_TYPE || "";
   let response;
+
   switch (pageType) {
     case "mctekk":
-      response = await api.post(
-        "https://graphql.datocms.com/",
-        {
-          query: `            query {
-        mctekkPost(filter: { slug: { eq: "${id}" } }) {
-          title
-          subdesc
-          image{url}
-    
-        }
-      }`,
-        },
-        {
-          headers: { Authorization: `2a23696ee4a5978719f9205c429acb` },
-          timeout: 5000,
-        }
-      );
-      break;
     case "gewaer":
-      response = await api.post(
-        "https://graphql.datocms.com/",
-        {
-          query: `            query {
-          gewaerPost(filter: { slug: { eq: "${id}" } }) {
-            title
-            subdesc
-            image{url}
-      
-          }
-        }`,
-        },
-        {
-          headers: { Authorization: `2a23696ee4a5978719f9205c429acb` },
-          timeout: 5000,
-        }
-      );
-      break;
     case "kanvas":
-      response = await api.post(
-        "https://graphql.datocms.com/",
-        {
-          query: `            query {
-          kanvasPost(filter: { slug: { eq: "${id}" } }) {
-            title
-            subdesc
-            image{url}
-      
+      try {
+        response = await api.post(
+          "https://graphql.datocms.com/",
+          {
+            query: `
+              query {
+                ${pageType}Post(filter: { slug: { eq: "${id}" } }) {
+                  title
+                  subdesc
+                  image { url }
+                }
+              }
+            `,
+          },
+          {
+            headers: { Authorization: `2a23696ee4a5978719f9205c429acb` },
+            timeout: 5000,
           }
-        }`,
+        );
+
+        const previousImages =
+          response.data?.data?.[`${pageType}Post`]?.image?.url;
+
+        return {
+          title: response.data?.data?.[`${pageType}Post`]?.title || "",
+          description: response.data?.data?.[`${pageType}Post`]?.subdesc || "",
+          openGraph: {
+            images: previousImages ? [previousImages] : [],
+          },
+        };
+      } catch (error) {
+        console.error("Error fetching post details:", error);
+        return {
+          title: "",
+          description: "",
+          openGraph: {
+            images: [],
+          },
+        };
+      }
+    default:
+      console.log("PAGE_TYPE not recognized");
+      console.log(pageType);
+      return {
+        title: "",
+        description: "",
+        openGraph: {
+          images: [],
         },
-        {
-          headers: { Authorization: `2a23696ee4a5978719f9205c429acb` },
-          timeout: 5000,
-        }
-      );
-      break;
+      };
   }
-
-  const previousImages = response?.data?.data?.pageTypePost?.image.url;
-
-  return {
-    title: response?.data?.data?.pageTypePost?.title,
-    description: response?.data?.data?.pageTypePost?.subdesc,
-    openGraph: {
-      images: [previousImages],
-    },
-  };
 }
+
 export default function Post({ params, searchParams }: Props) {
   return (
     <div>
