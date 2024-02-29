286cd012c431cd696806e56957aba68b09b6270b
José David Sánchez <49158320+Arzoid29@users.noreply.github.com>
2024-02-29 14:39:25 -0400

    feat: new blog cards

--- src/components/atoms/postCards/index.tsx
+++ src/components/atoms/postCards/index.tsx
@@ -2,7 +2,6 @@ import Link from "next/link";
 import React from "react";
 import { BsArrowUpRight } from "react-icons/bs";
 
-
 export interface Imagen {
   url: string;
 }
@@ -22,24 +21,29 @@ export interface PostCardProps {
 function PostCard({ post }: PostCardProps): JSX.Element {
   return (
     <Link href={`/posts/${encodeURIComponent(post.slug)}`}>
-      <div className=" md:w-full lg:w-full xl:w-full  w-full p-4 shadow-2xl mb-14 ">
-        <div className=" overflow-hidden  transition-transform transform ">
+      <div className="md:w-full lg:w-full xl:w-full w-full p-4 shadow-2xl mb-14 relative">
+        <img
+          src={post.image.url}
+          alt={post.title}
+          className="w-full h-auto mb-4 object-cover object-center"
+          style={{ maxHeight: "180px" }}
+        />
+
+        <div className="overflow-hidden transition-transform transform">
           <div className="p-4">
-            <div className="flex-row flex justify-between">
-              <div>
-                {" "}
-                <p className=" font-bold mb-2 text-[1.5rem] hover:text-gray-600 ">
-                  {post.title}
-                </p>
-              </div>
+            <div className="flex justify-between items-center">
+              <p className="font-bold text-2xl mb-2 text-black">
+                {post.title}
+              </p>
               <div className="mt-4">
-                <BsArrowUpRight/>
+                <BsArrowUpRight />
               </div>
             </div>
+
             {post.subdesc && (
-              <p className="text-[1rem] text-gray-500 mb-4">{post.subdesc}</p>
+              <p className="text-lg text-gray-500 mb-4">{post.subdesc}</p>
             )}
-            <p className="text-gray-800 text-[18px]">
+            <p className="text-gray-800 text-lg">
               {new Date(post._firstPublishedAt).toLocaleDateString()}
             </p>
           </div>
@@ -47,6 +51,6 @@ function PostCard({ post }: PostCardProps): JSX.Element {
       </div>
     </Link>
   );
-};
+}
 
 export default PostCard;
