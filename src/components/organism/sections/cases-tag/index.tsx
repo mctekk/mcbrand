4197a8e972d0295680b9b60e07b61ad311c96349
José David Sánchez <49158320+Arzoid29@users.noreply.github.com>
2024-03-19 17:26:41 -0400

    fix: spaces

--- src/components/organism/sections/cases-tag/index.tsx
+++ src/components/organism/sections/cases-tag/index.tsx
@@ -1,24 +1,24 @@
-import { Section } from '@/components/atoms/section';
-import React from 'react';
+import React from "react";
+import { Section } from "@/components/atoms/section";
 
 interface TituloDescripcion {
   titulo: string;
   descripcion: string;
 }
 
-const Tags: React.FC<{ datos: TituloDescripcion[] }> = ({ datos }) => {
+function Tags({ datos }: { datos: TituloDescripcion[] }) {
   return (
     <Section>
-    <div className="flex flex-row justify-between section ml-24">
-      {datos.map((item, index) => (
-        <div key={index} className="w-1/4">
-          <h2 className="text-xl font-bold mb-2">{item.titulo}</h2>
-          <p>{item.descripcion}</p>
-        </div>
-      ))}
-    </div>
+      <div className="flex flex-col md:flex-row md:justify-between section md:ml-24 w-fit lg:w-full mx-auto text-center lg:text-justify">
+        {datos.map((item, index) => (
+          <div key={index} className="w-full md:w-1/4 md:mr-4 mb-4 md:mb-0">
+            <h2 className="text-xl font-bold mb-2">{item.titulo}</h2>
+            <p className="">{item.descripcion}</p>
+          </div>
+        ))}
+      </div>
     </Section>
   );
-};
+}
 
 export default Tags;
