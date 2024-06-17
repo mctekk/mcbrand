924ad6b85f3c757f90fb78a92f9fe28021af9823
José David Sánchez <49158320+Arzoid29@users.noreply.github.com>
2024-06-17 15:48:58 -0400

    feat: accept excel

--- src/components/atoms/form-cv/index.tsx
+++ src/components/atoms/form-cv/index.tsx
@@ -204,7 +204,7 @@ const ContactFormCv: React.FC = () => {
             type="file"
             id="resume"
             name="resume"
-            accept=".pdf, .doc, .docx, .jpg, .png, .excel"
+            accept=".pdf, .doc, .docx, .jpg, .png, .xlsx"
             onChange={handleFileChange}
             className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500"
             required
