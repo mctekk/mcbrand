7ffddbcc72327f80f50ef4a33c79e82d88f9c6fe
Maximo Castro Pou <max@mctekk.com>
2026-06-17 10:09:30 -0400

    hotfix redirect

--- next.config.js
+++ next.config.js
@@ -2,7 +2,17 @@
 const nextConfig = {
     images: {
         domains: ['www.datocms-assets.com'],
-    }
+    },
+    async redirects() {
+        return [
+            {
+                source: '/:path*',
+                has: [{ type: 'host', value: 'dev.kanvas.dev' }],
+                destination: 'https://kanvas.dev/',
+                permanent: true,
+            },
+        ]
+    },
 }
 
 module.exports = nextConfig
